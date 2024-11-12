a0460c281473b6d9c616bf41383c2901328b2950
AndersonHJB <bornforthis@bornforthis.cn>
2024-11-12 16:10:23 +0800

    改进订阅页面

--- Test/Generate.py
+++ Test/Generate.py
@@ -11,7 +11,8 @@ def generate_text(paths):
         with open(os.path.join(base_root, path), 'r') as f:
             content = f.read()
             text += filename + ':\n' + content + '\n'
-    with open('data/text.txt', 'a+') as f:
+    # with open('data/text.txt', 'a+') as f:
+    with open('data/text.txt', 'w') as f:
         f.write(text)
 
 if __name__ == '__main__':
@@ -37,8 +38,8 @@ if __name__ == '__main__':
             'static/base.css',
         ],
     }
-    for paths in data_dict.values():
-        # print(path)
-        generate_text(paths)
+    # for paths in data_dict.values():
+    #     # print(path)
+    #     generate_text(paths)
 
-    # generate_text(data_dict['subscription'])
+    generate_text(data_dict['subscription'])
